905f9cecbb187ccb3e15b3c7ce218cdaa60cc287
acelee0621 <acelee0621@outlook.com>
2025-10-22 12:04:34 +0800

    使用alembic做数据库迁移功能

--- src/lifespan.py
+++ src/lifespan.py
@@ -12,7 +12,7 @@ from src.core.database import create_db_and_tables
 async def lifespan(app: FastAPI) -> AsyncIterator:
     # -------- 启动 --------
     logger.info("应用启动，开始加载所有资源...")
-    await create_db_and_tables()
+    # await create_db_and_tables()
 
     # -------- 运行 --------
     yield
